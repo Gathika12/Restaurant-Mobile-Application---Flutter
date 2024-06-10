615a07544205a0c92a30e96c5037222b75ad66c7
Gathika12 <gathikacolambage@gmail.com>
2024-06-10 12:47:11 -0700

    .

--- restaurant_mobile_application/lib/welcome.dart
+++ restaurant_mobile_application/lib/welcome.dart
@@ -1,1 +1,52 @@
+import 'package:flutter/material.dart';
 
+class Welcome extends StatefulWidget {
+  @override
+  WelcomePage createState() => WelcomePage();
+}
+
+class WelcomePage extends State<Welcome> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/burger.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Column(
+            children: <Widget>[
+              AppBar(
+                title: Text(
+                  'Login In To Your Account',
+                  style: TextStyle(color: Colors.black),
+                ),
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+              ),
+              Expanded(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      // Your form or other widgets can go here
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
