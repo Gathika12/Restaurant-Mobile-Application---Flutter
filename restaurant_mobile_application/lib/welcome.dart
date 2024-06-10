051566ed2a55e760325f9860f521e9ab1552da03
Gathika12 <gathikacolambage@gmail.com>
2024-06-10 13:38:07 -0700

    .

--- restaurant_mobile_application/lib/welcome.dart
+++ restaurant_mobile_application/lib/welcome.dart
@@ -23,6 +23,9 @@ class WelcomePage extends State<Welcome> {
               ),
             ),
           ),
+          Container(
+            color: Colors.grey.withOpacity(0.5), // Semi-transparent overlay
+          ),
           Column(
             children: <Widget>[
               AppBar(
@@ -38,7 +41,63 @@ class WelcomePage extends State<Welcome> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      // Your form or other widgets can go here
+                      TextFormField(
+                          //controller: _passwordController,
+                          decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Enter your Email',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 3.0),
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.blue,
+                              width:
+                                  3.0), // Increased border width when focused
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                      )),
+                      SizedBox(height: 20),
+                      TextFormField(
+                          //controller: _passwordController,
+                          decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Enter your Email',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 3.0),
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.blue,
+                              width:
+                                  3.0), // Increased border width when focused
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                      )),
+                      SizedBox(height: 20),
+                      Center(
+                          child: ElevatedButton(
+                              child: Text(
+                                "Log In",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.red,
+                                fixedSize: Size(150, 30),
+                                elevation: 0,
+                              ),
+                              onPressed: () async {
+                                /*FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                                  email: _emailController.text,
+                                  password: _passwordController.text)
+                              .then((value) {*/
+                              }))
                     ],
                   ),
                 ),
